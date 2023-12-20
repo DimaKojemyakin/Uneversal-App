575ee00ca0e48eb2ddd7c0db768cfa545acdd07b
dima <dima@kojem.ru>
2023-12-20 22:36:03 +0300

    1

--- Uneversal App/Data/UserDate.swift
+++ Uneversal App/Data/UserDate.swift
@@ -1,20 +1,42 @@
-// UserManager.swift
 import Foundation
 
 class UserManager: ObservableObject {
     @Published var user = User()
-
-    func updateUser(userName: String, userPassword: String, isRegistered: Bool) {
+    
+    func updateUser(userName: String, userPassword: String, isRegistered: Bool, lessonM: [lessons]) {
         DispatchQueue.main.async {
             self.user.userName = userName
             self.user.userPassword = userPassword
             self.user.isRegistred = isRegistered
+            self.user.lessonsM = lessonM.enumerated().map { index, lesson in
+                return LessonWithIndex(index: index, lesson: lesson)
+            }
         }
     }
+}
+
+struct lessons: Codable, Hashable {
+    let nameTeacher: String
+    let nameLesson: String
+    let classRoom: String
+}
+
+struct LessonWithIndex: Codable, Hashable {
+    let index: Int
+    let lesson: lessons
 }
 
 struct User: Codable {
     var userName = ""
     var userPassword = ""
     var isRegistred = false
+    
+    var lessonsM: [LessonWithIndex] = [
+        LessonWithIndex(index: 0, lesson: lessons(nameTeacher: "Екатерина Дмитриевна", nameLesson: "Разговоры о важном", classRoom: "120")),
+        LessonWithIndex(index: 1, lesson: lessons(nameTeacher: "Владимир Юрьевичь", nameLesson: "Технология", classRoom: "120")),
+        LessonWithIndex(index: 2, lesson: lessons(nameTeacher: "Владимир Юрьевичь", nameLesson: "Технология", classRoom: "120")),
+        LessonWithIndex(index: 3, lesson: lessons(nameTeacher: "Владимир Юрьевичь", nameLesson: "Технология", classRoom: "120")),
+        LessonWithIndex(index: 4, lesson: lessons(nameTeacher: "Владимир Юрьевичь", nameLesson: "Технология", classRoom: "120")),
+        LessonWithIndex(index: 5, lesson: lessons(nameTeacher: "Владимир Юрьевичь", nameLesson: "Технология", classRoom: "120")),
+    ]
 }
--- Uneversal App/View/MainView.swift
+++ Uneversal App/View/MainView.swift
@@ -5,37 +5,57 @@ struct MainView: View {
     @EnvironmentObject var dataSettings: DataSettings
     @EnvironmentObject var dataManager: DataManager
     
-    
     @State private var isActiveSheetSettings = false
-    @State private var isImagePickerPresented = false
+    @State private var isChangeLessons = false
     @State private var selectedImage: UIImage?
     
     var body: some View {
         NavigationView {
-            TabView{
-                LoveIS()
-                    .badge("!")
-                    .tabItem { Label("Love is", systemImage: "heart") }
-                LoveIS()
-                    .badge("!")
-                    .tabItem { Label("Calculator", systemImage: "faxmachine") }
+            VStack {
+                List {
+                    Section(header: Text("Понедельник")) {
+                        ForEach(userManager.user.lessonsM, id: \.self.index) { lesson in
+                            HStack {
+                                Text("\(lesson.index + 1).")
+                                    .fontWeight(.bold)
+                                Text(lesson.lesson.nameLesson).fontWeight(.bold)
+                                Spacer()
+                                VStack {
+                                    Text(lesson.lesson.nameTeacher)
+                                        .font(.caption2)
+                                    Text(lesson.lesson.classRoom).fontWeight(.light)
+                                }
+                            }
+                        }
+                    }
+                }
             }
             .sheet(isPresented: $isActiveSheetSettings) {
                 Settings()
+            }
+            .sheet(isPresented: $isChangeLessons){
+                ChangeLesson()
             }
             .navigationBarItems(leading: HStack {
                 Button(action: { isActiveSheetSettings.toggle() }) {
                     Image(systemName: "gearshape")
                 }
+            }, trailing:
+                                    HStack{
+                Button(action: { isChangeLessons.toggle() }){
+                    Image(systemName: "plus.app")
+                }
             }
-                                
             )
+            .navigationTitle("Lessons")
         }
     }
 }
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView().environmentObject(UserManager()).environmentObject(DataManager()).environmentObject(DataSettings())
     }
 }
+
