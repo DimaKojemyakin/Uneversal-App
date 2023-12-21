a2613bbddc391cae95b76fc0a7e836b97218d7b5
dima <dima@kojem.ru>
2023-12-21 20:51:13 +0300

    2

---
+++ Uneversal App/View/AddNewLesson.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewLesson.swift
+//  Uneversal App
+//
+//  Created by Дима Кожемякин on 21.12.2023.
+//
+
+import SwiftUI
+
+struct AddNewLesson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewLesson()
+}
